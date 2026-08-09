95179140333c48bd41746739848d434bbceaa80b
Veenit Sahay <82210241+veenitsahay@users.noreply.github.com>
2026-08-10 00:19:33 +0100

    Add: binary-search - submission-18

---
+++ Data Structures & Algorithms/binary-search/submission-18.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        // split the list from middle 
+        // if any half is last element is less than target discard
+        // if the first element is less and last element is greater 
+        // than split in half again
+        int j = 0;
+        int k = nums.length -1 ;
+
+        while(j <= k){
+            int mid = j + (k - j)/2;
+
+            if(nums[mid] < target){ 
+                j = mid + 1;
+            }else if(nums[mid] > target){
+                k = mid - 1;
+            }else{
+                 return mid;
+            }
+        }
+        return -1;
+
+    }
+}
