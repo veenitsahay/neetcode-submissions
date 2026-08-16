170261fff2a2aab8813341800e236bb452648157
Veenit Sahay <82210241+veenitsahay@users.noreply.github.com>
2026-08-17 00:02:14 +0100

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ret = new int[n];
+
+        int l = 0, r = 0;
+
+        while(r < n ){
+
+            if(temperatures[l] < temperatures[r]){
+                ret[l] = r - l;
+                l++;
+                r = l;
+            }else{
+                if(r == n -1 ){
+                    ret[l] = 0;
+                    l++;
+                    r = l;
+                }else{
+                    r++;
+                }
+            }
+        }
+        System.gc();
+        return ret;
+    }
+}
