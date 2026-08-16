f27d16c44d6e87432065dc83fc4c9d7706b17dd8
Veenit Sahay <82210241+veenitsahay@users.noreply.github.com>
2026-08-16 10:36:19 +0100

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int len = numbers.length, l = 0, r = len - 1;
+
+
+        //binary search algo I guess
+        // divide array by 2
+        // last element of first half is less than and first element 
+
+        while(l < r){
+
+            if(numbers[r] + numbers[l] < target){
+                l++;
+            }else if(numbers[l] + numbers[r] > target){
+                r--;
+            }else{
+                return new int[]{l+1, r+1};
+            }
+        }
+
+        return null;
+    }
+}
