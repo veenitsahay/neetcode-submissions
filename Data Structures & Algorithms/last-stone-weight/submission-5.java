a5e905d3081f0f4fcdf9d7d16f9585145aad25c0
Veenit Sahay <82210241+veenitsahay@users.noreply.github.com>
2026-09-06 23:11:40 +0100

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+//9,7,6
+        for(int num : stones){
+            maxHeap.offer(num);
+        }
+
+        while(maxHeap.size() > 1){
+            maxHeap.offer(Math.abs(maxHeap.poll() - maxHeap.poll()));
+        }
+
+        return maxHeap.poll();
+    }
+}
